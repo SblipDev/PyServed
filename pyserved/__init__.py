cdc426d6623a37328b3f97071bf3281185a7643f
shaurya-blip <shaurya.p.singh21@gmail.com>
2022-03-21 12:14:19 +0530

    Added logo and a welcome program.

--- pyserved/__init__.py
+++ pyserved/__init__.py
@@ -1,17 +1,40 @@
 """
+_____        _____                         _ 
+|  __ \      / ____|                       | |
+| |__) |   _| (___   ___ _ ____   _____  __| |
+|  ___/ | | |\___ \ / _ \ '__\ \ / / _ \/ _` |
+| |   | |_| |____) |  __/ |   \ V /  __/ (_| |
+|_|    \__, |_____/ \___|_|    \_/ \___|\__,_|
+        __/ |                                 
+        |___/                                  
 
-Pyserved module
-
-1.1 -> Added support for all OS
-1.2 -> Removed cli and just made a python package.
-MAJOR UPDATE
-2.0 -> Bought back cli and added support for ALL codec files. 
-2.0.1 -> Fixed bug with codecs.
-2.0.2 -> Added colors! and error messages for when something goes wrong.
-2.0.3 -> Fixed MASSIVE bug of v2.0.2
 Shaurya Pratap Singh 2021
 """
 
 __version__ = '2.0.5'
-__author__ = 'Shaurya Pratap Singh'
-__message__ = "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs"
+__author__ = 'SblipDev'
+__github__ = "https://github.com/SblipDev/PyServed"
+__logo__ = """
+_____        _____                         _ 
+|  __ \      / ____|                       | |
+| |__) |   _| (___   ___ _ ____   _____  __| |
+|  ___/ | | |\___ \ / _ \ '__\ \ / / _ \/ _` |
+| |   | |_| |____) |  __/ |   \ V /  __/ (_| |
+|_|    \__, |_____/ \___|_|    \_/ \___|\__,_|
+        __/ |                                 
+        |___/  
+"""
+
+
+def main():
+    print(__logo__)
+    print("PyServed v{}".format(__version__))
+    print("Author: {0}".format(__author__))
+    print("Github: {0}".format(__github__))
+    print("")
+
+if __name__ == '__main__':
+    main()
+else:
+    pass
+
