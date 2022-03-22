f49ad98c8aef6176927b76d70eee1c293d9b71ca
shaurya-blip <shaurya.p.singh21@gmail.com>
2022-03-22 20:26:34 +0530

    EDIT setup.py to add pdhost binary and bump version.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-import pathlib
+from pathlib import Path
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyserved",
-    version="2.0.5",
-    description="Share files with your friends (on the same network though). Project setup by Choam by Kian Mckenna.",
+    version="2.0.6",
+    description="Network transfer/hosting utilities by SblipDev.",
     long_description_content_type="text/markdown",
+    long_description=long_description,
     url="https://github.com/SblipDev/pyserved/",
     author="Shaurya Pratap Singh",
     author_email="shaurya.p.singh21@gmail.com",
@@ -13,5 +17,5 @@ setup(
     packages=["pyserved"],
     install_requires=['netifaces', 'rich'],
     include_package_data=True,
-    scripts=['pyserved/bin/pdlisten', 'pyserved/bin/pdsnd'],
+    scripts=['pyserved/bin/pdlisten', 'pyserved/bin/pdsnd', 'pyserved/bin/pdhost'],
 )
